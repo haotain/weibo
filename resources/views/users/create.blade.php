8aa1956945d72dde98177d3c1ed1a4fb9c51c995
吴守雍 <872789604@qq.com>
2021-01-12 17:45:04 +0800

    用户注册表单

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -2,5 +2,32 @@
 @section('title', '注册')
 
 @section('content')
-<h1>注册</h1>
+  <div class="offset-md-2 col-md-8">
+    <div class="card">
+      <div class="card-header">
+       <h1>注册</h1>
+      </div>
+      <div class="card-body">
+        <form method="post" action="{{ route('users.store') }}">
+          <div class="form-group">
+            <label for="name">名称：</label>
+            <input type="text"  name="name" class="form-control" id="name" value="{{ old('name') }}">
+          </div>
+          <div class="form-group">
+            <label for="email">邮箱：</label>
+            <input type="text" name="email" class="form-control" id="email" value="{{ old('email') }}">
+          </div>
+          <div class="form-group">
+            <label for="password">密码：</label>
+            <input type="password" name="password" class="form-control" id="password" value="{{ old('password') }}">
+          </div>
+          <div class="form-group">
+            <label for="password_confirmation">确认密码：</label>
+            <input type="password" name="password_confirmation" class="form-control" id="password_confirmation" value="{{ old('password_confirmation') }}">
+          </div>
+          <button type="submit" class="btn btn-primary">注册</button>
+        </form>
+      </div>
+    </div>
+  </div>
 @stop
