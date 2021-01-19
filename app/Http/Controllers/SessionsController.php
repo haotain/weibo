0f7d5fc502526cc820341bdf539e0cf32637decd
吴守雍 <872789604@qq.com>
2021-01-19 16:28:17 +0800

    登录时记住我

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -24,7 +24,7 @@ class SessionsController extends Controller
             'email' => 'required|email|max:255',
             'password' => 'required'
         ]);
-        if (Auth::attempt($credentials)) {
+        if (Auth::attempt($credentials,  $request->has('remember'))) {
             session()->flash('success', '欢迎回来！');
             return redirect()->route('users.show', [Auth::user()]);
         }
@@ -32,5 +32,14 @@ class SessionsController extends Controller
         return redirect()->back()->withInput();
     }
 
+    /**
+     * 退出登录
+     */
+    public function destroy()
+    {
+        Auth::logout();
+        session()->flash('success', '您已成功退出！');
+        return redirect('login');
+    }
 
 }
